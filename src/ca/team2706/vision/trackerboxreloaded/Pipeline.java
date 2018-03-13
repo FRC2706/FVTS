93f547ed2b7e0f6858bd99c514ff1a915d7d7a39
Mike Ounsworth <ounsworth@gmail.com>
2018-03-12 20:52:20 -0400

    Log binary mask images

--- src/ca/team2706/vision/trackerboxreloaded/Pipeline.java
+++ src/ca/team2706/vision/trackerboxreloaded/Pipeline.java
@@ -54,7 +54,7 @@ public class Pipeline {
 		Imgproc.dilate(erodeOne, dilated, new Mat(), new Point(), 2*visionParams.erodeDilateIterations, Core.BORDER_CONSTANT, new Scalar(0));
 	    Imgproc.erode(dilated, erodeTwo, new Mat(), new Point(), visionParams.erodeDilateIterations, Core.BORDER_CONSTANT, new Scalar(0));
 
-		visionData.binMask = erodeTwo;
+		visionData.binMask = erodeTwo.clone();
 
 		//Find contours
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
