dcac0d80c2bf5d740d1274ef107556517b02a15e
thatmadhacker <finman292004@gmail.com>
2019-02-02 12:04:13 -0500

    Changed logging folder

--- src/main/java/ca/team2706/vision/vision2019/Logger.java
+++ src/main/java/ca/team2706/vision/vision2019/Logger.java
@@ -5,45 +5,12 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class Logger {
-
-	private static File LOG_FOLDER = new File("logs/");
-
-	private static int cutoff = 400;
-
-	private static int currSize = 0;
-	private static int logFileNum = 0;
-
-	public static void init() {
-
-		if (!LOG_FOLDER.exists()) {
-			LOG_FOLDER.mkdirs();
-		}
-
-		int highestVal = -1;
-
-		for (File f : LOG_FOLDER.listFiles()) {
-
-			if (f.getName().endsWith(".log")) {
-
-				int name = Integer.valueOf(f.getName().substring(0, f.getName().indexOf(".log")));
-
-				if (name > highestVal) {
-					highestVal = name;
-				}
-
-			}
-
-		}
-
-		logFileNum = highestVal + 1;
-
-	}
-
-	private static void log(String log) {
+	
+	private static void log(String log,String folder) {
 
 		try {
 
-			File logFile = new File(LOG_FOLDER, logFileNum + ".log");
+			File logFile = new File(folder,"log.log");
 
 			if(!logFile.exists()) {
 				
@@ -58,14 +25,6 @@ public class Logger {
 
 			out.close();
 
-			currSize++;
-
-			if (currSize > cutoff) {
-
-				logFileNum++;
-
-			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -74,27 +33,27 @@ public class Logger {
 
 	}
 
-	public static void i(String log) {
+	public static void i(String log, String folder) {
 
-		log("Info: " + log);
-
-	}
-
-	public static void w(String log) {
-
-		log("Warn: " + log);
+		log("Info: " + log,folder);
 
 	}
 
-	public static void e(String log) {
+	public static void w(String log, String folder) {
 
-		log("Error: " + log);
+		log("Warn: " + log,folder);
 
 	}
 
-	public static void d(String log) {
+	public static void e(String log, String folder) {
 
-		log("Debug: " + log);
+		log("Error: " + log,folder);
+
+	}
+
+	public static void d(String log, String folder) {
+
+		log("Debug: " + log,folder);
 
 	}
 
--- src/main/java/ca/team2706/vision/vision2019/Main.java
+++ src/main/java/ca/team2706/vision/vision2019/Main.java
@@ -280,7 +280,7 @@ public class Main {
 			}
 		} catch (Exception e1) {
 			e1.printStackTrace();
-			Logger.e("\n\nError reading the params file, check if the file is corrupt?");
+			Logger.e("\n\nError reading the params file, check if the file is corrupt?",outputPath);
 			System.exit(1);
 		}
 	}
@@ -484,7 +484,7 @@ public class Main {
 
 			if (!camera.isOpened()) {
 				// If the camera didn't open throw an error
-				Logger.e("Error: Can not connect to camera");
+				Logger.e("Error: Can not connect to camera",outputPath);
 				// Exit
 				System.exit(1);
 			}
@@ -538,7 +538,7 @@ public class Main {
 			if (useCamera) {
 				// Read the frame from the camera, if it fails try again
 				if (!camera.read(frame)) {
-					Logger.e("Error: Failed to get a frame from the camera");
+					Logger.e("Error: Failed to get a frame from the camera",outputPath);
 					continue;
 				}
 			} // else use the image from disk that we loaded above
@@ -601,7 +601,7 @@ public class Main {
 					continue;
 				} catch (NullPointerException e) {
 					e.printStackTrace();
-					Logger.w("Window closed");
+					Logger.w("Window closed",outputPath);
 					Runtime.getRuntime().halt(0);
 				} catch (Exception e) {
 					// just in case
@@ -635,7 +635,7 @@ public class Main {
 			}
 			// Display the frame rate onto the console
 			double pipelineTime = (((double) (pipelineEnd - pipelineStart)) / Pipeline.NANOSECONDS_PER_SECOND) * 1000;
-			Logger.i("Vision FPS: "+visionData.fps+", pipeline took: "+pipelineTime+" ms\n");
+			Logger.i("Vision FPS: "+visionData.fps+", pipeline took: "+pipelineTime+" ms\n",outputPath);
 		}
 	} // end main video processing loop
 
